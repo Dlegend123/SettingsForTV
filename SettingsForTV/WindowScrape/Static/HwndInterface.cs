b168e07e59d1e67edc2820c5f75998d4be07b5b8
Mark Morrison <mamorrison@etechja.com>
2022-09-17 23:07:22 -0500

    Add project files.

---
+++ SettingsForTV/WindowScrape/Static/HwndInterface.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using SettingsForTV.WindowScrape.Constants;
+using SettingsForTV.WindowScrape.Types;
+
+namespace SettingsForTV.WindowScrape.Static;
+
+public class HwndInterface
+{
+    [DllImport("user32.dll")]
+    public static extern bool CloseWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    public static extern bool EnumDisplaySettingsA(string deviceName, int modeNum, ref DEVMODE1 devMode);
+
+    [DllImport("user32.dll")]
+    public static extern int ChangeDisplaySettings(ref DEVMODE1 devMode, int flags);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className,
+        string windowTitle);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    [DllImport("user32.Dll")]
+    public static extern int EnumWindows(EnumWindowsProc x, int y);
+
+    public static IntPtr GetHwnd(string windowText, string className)
+    {
+        return (IntPtr)FindWindow(className, windowText);
+    }
+
+    public static IntPtr GetHwndChild(IntPtr hwnd, string clsName, string ctrlText)
+    {
+        return FindWindowEx(hwnd, IntPtr.Zero, clsName, ctrlText);
+    }
+
+    public static string GetHwndClassName(IntPtr hwnd)
+    {
+        var lpClassName = new StringBuilder(0x100);
+        _ = GetClassName(hwnd, lpClassName, lpClassName.MaxCapacity);
+        return lpClassName.ToString();
+    }
+
+    public static IntPtr GetHwndFromClass(string className)
+    {
+        return (IntPtr)FindWindow(className, null);
+    }
+
+    public static IntPtr GetHwndFromTitle(string windowText)
+    {
+        return (IntPtr)FindWindow(null, windowText);
+    }
+
+    public static IntPtr GetHwndParent(IntPtr hwnd)
+    {
+        return GetParent(hwnd);
+    }
+
+    public static Point GetHwndPos(IntPtr hwnd)
+    {
+        GetWindowRect(hwnd, out var lpRect);
+        return new Point(lpRect.Left, lpRect.Top);
+    }
+
+    public static Size GetHwndSize(IntPtr hwnd)
+    {
+        GetWindowRect(hwnd, out var lpRect);
+        return new Size(lpRect.Right - lpRect.Left, lpRect.Bottom - lpRect.Top);
+    }
+
+    public static string GetHwndText(IntPtr hwnd)
+    {
+        var capacity = (int)SendMessage(hwnd, 14, 0, 0) + 1;
+        var lParam = new StringBuilder(capacity);
+        SendMessage(hwnd, 13, (uint)capacity, lParam);
+        return lParam.ToString();
+    }
+
+    public static string GetHwndTitle(IntPtr hwnd)
+    {
+        var lpString = new StringBuilder(GetHwndTitleLength(hwnd) + 1);
+        _ = GetWindowText(hwnd, lpString, lpString.Capacity);
+        return lpString.ToString();
+    }
+
+    public static int GetHwndTitleLength(IntPtr hwnd)
+    {
+        return GetWindowTextLength(hwnd);
+    }
+
+    public static int GetMessageInt(IntPtr hwnd, WM msg)
+    {
+        return (int)SendMessage(hwnd, (uint)msg, 0, 0);
+    }
+
+    public static string GetMessageString(IntPtr hwnd, WM msg, uint param)
+    {
+        var lParam = new StringBuilder(0x10000);
+        SendMessage(hwnd, (uint)msg, param, lParam);
+        return lParam.ToString();
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    public static extern IntPtr GetParent(IntPtr hWnd);
+
+    [return: MarshalAs(UnmanagedType.Bool)]
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool IsProcessCritical(IntPtr hProcess, ref bool Critical);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern int GetWindowTextLength(IntPtr hWnd);
+
+    [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+    public static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern uint GetSecurityInfo(IntPtr handle, SE_OBJECT_TYPE objectType,
+        SECURITY_INFORMATION securityInfo, out IntPtr sidOwner, out IntPtr sidGroup, out IntPtr dacl, out IntPtr sacl,
+        out IntPtr securityDescriptor);
+
+    [DllImport("user32.dll")]
+    public static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    public static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+    [DllImport("user32.Dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool EnumChildWindows(IntPtr parentHandle, Win32Callback callback, IntPtr lParam);
+
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, string lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, StringBuilder lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+
+    [DllImport("user32.dll")]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
+        uint uFlags);
+
+    [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+    public static extern bool SetWindowText(IntPtr hWnd, string lpString);
+
+    [return: MarshalAs(UnmanagedType.Bool)]
+    [DllImport("user32.dll")]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    public static int SendMessage(IntPtr hwnd, WM msg, uint param1, string param2)
+    {
+        return (int)SendMessage(hwnd, (uint)msg, param1, param2);
+    }
+
+    public static int SendMessage(IntPtr hwnd, WM msg, uint param1, uint param2)
+    {
+        return (int)SendMessage(hwnd, (uint)msg, param1, param2);
+    }
+
+    public static bool MinimizeWindow(IntPtr hwnd)
+    {
+        return CloseWindow(hwnd);
+    }
+
+    public static bool SetHwndPos(IntPtr hwnd, int x, int y)
+    {
+        return SetWindowPos(hwnd, IntPtr.Zero, x, y, 0, 0, 5);
+    }
+
+    public static bool WindowVisible(IntPtr hwnd)
+    {
+        return IsWindowVisible(hwnd);
+    }
+
+    public static bool GetEnumChildWindows(IntPtr parent, Win32Callback childProc, IntPtr listHandle)
+    {
+        return EnumChildWindows(parent, childProc, listHandle);
+    }
+
+    public static bool SetHwndSize(IntPtr hwnd, int w, int h)
+    {
+        return SetWindowPos(hwnd, IntPtr.Zero, 0, 0, w, h, 6);
+    }
+
+    public static void SetHwndText(IntPtr hwnd, string text)
+    {
+        SendMessage(hwnd, 12, 0, text);
+    }
+
+    public static bool SetHwndTitle(IntPtr hwnd, string text)
+    {
+        return SetWindowText(hwnd, text);
+    }
+
+
+    public static bool ActivateWindow(IntPtr hwnd)
+    {
+        return SetForegroundWindow(hwnd);
+    }
+
+    public static void ClickHwnd(IntPtr hwnd)
+    {
+        SendMessage(hwnd, 0xf5, IntPtr.Zero, IntPtr.Zero);
+    }
+
+    #region AlignTop
+
+    public static bool AlignTopCenter(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(screen.Left + screen.Width / 2 - (rct.Right - rct.Left) / 2,
+            0);
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignTopRight(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(screen.Width - (rct.Right - rct.Left), 0);
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignTopLeft(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out _);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var pt = new Point(0, 0);
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    #endregion
+
+    #region AlignCenter
+
+    public static bool AlignCenter(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(screen.Left + screen.Width / 2 - (rct.Right - rct.Left) / 2,
+            screen.Top + screen.Height / 2 - (rct.Bottom - rct.Top) / 2);
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignCenterLeft(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(0, screen.Top + screen.Height / 2 - (rct.Bottom - rct.Top) / 2);
+        return SetWindowPos(ptr, position, 0, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignCenterRight(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+
+        var pt = new Point(screen.Width - (rct.Right - rct.Left),
+            screen.Top + screen.Height / 2 - (rct.Bottom - rct.Top) / 2);
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    #endregion
+
+    #region AlignBottom
+
+    public static bool AlignBottomLeft(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(0, screen.Height - (rct.Bottom - rct.Top));
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignBottomRight(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(screen.Width - (rct.Right - rct.Left), screen.Height - (rct.Bottom - rct.Top));
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    public static bool AlignBottomCenter(IntPtr ptr, IntPtr position)
+    {
+        GetWindowRect(ptr, out var rct);
+        if (ptr == IntPtr.Zero) return false;
+        // Move the window to (0,0) without changing its size or position
+        // in the Z order.
+        var screen = Screen.FromHandle(ptr).Bounds;
+        var pt = new Point(screen.Left + screen.Width / 2 - (rct.Right - rct.Left) / 2,
+            screen.Height - (rct.Bottom - rct.Top));
+        return SetWindowPos(ptr, position, pt.X, pt.Y, 0, 0,
+            (uint)(PositioningFlags.SWP_NOSIZE | PositioningFlags.SWP_NOZORDER));
+    }
+
+    #endregion
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DEVMODE1
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmDeviceName;
+
+        public short dmSpecVersion;
+        public short dmDriverVersion;
+        public short dmSize;
+        public short dmDriverExtra;
+        public int dmFields;
+
+        public short dmOrientation;
+        public short dmPaperSize;
+        public short dmPaperLength;
+        public short dmPaperWidth;
+
+        public short dmScale;
+        public short dmCopies;
+        public short dmDefaultSource;
+        public short dmPrintQuality;
+        public short dmColor;
+        public short dmDuplex;
+        public short dmYResolution;
+        public short dmTTOption;
+        public short dmCollate;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmFormName;
+
+        public short dmLogPixels;
+        public short dmBitsPerPel;
+        public int dmPelsWidth;
+        public int dmPelsHeight;
+
+        public int dmDisplayFlags;
+        public int dmDisplayFrequency;
+
+        public int dmICMMethod;
+        public int dmICMIntent;
+        public int dmMediaType;
+        public int dmDitherType;
+        public int dmReserved1;
+        public int dmReserved2;
+
+        public int dmPanningWidth;
+        public int dmPanningHeight;
+    }
+
+    public delegate int EnumWindowsProc(IntPtr hwnd, int lParam);
+
+    public delegate bool Win32Callback(IntPtr hwnd, IntPtr lParam);
+    public const int ENUM_CURRENT_SETTINGS = -1;
+    public const int CDS_UPDATEREGISTRY = 0x01;
+    public const int CDS_TEST = 0x02;
+    public const int DISP_CHANGE_SUCCESSFUL = 0;
+    public const int DISP_CHANGE_RESTART = 1;
+    public const int DISP_CHANGE_FAILED = -1;
+}
---
+++ SettingsForTV/WindowScrape/Types/HwndObject.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+using SettingsForTV.WindowScrape.Constants;
+using SettingsForTV.WindowScrape.Static;
+using static SettingsForTV.WindowScrape.Static.HwndInterface;
+
+namespace SettingsForTV.WindowScrape.Types;
+
+public class HwndObject
+{
+    private readonly List<IntPtr> results = new();
+
+    public HwndObject(IntPtr hwnd)
+    {
+        Hwnd = hwnd;
+    }
+
+    public HwndObject()
+    {
+    }
+
+    public string ClassName => HwndInterface.GetHwndClassName(Hwnd);
+
+    public IntPtr Hwnd { get; }
+
+    public Point Location
+    {
+        get => HwndInterface.GetHwndPos(Hwnd);
+        set => HwndInterface.SetHwndPos(Hwnd, value.X, value.Y);
+    }
+
+    public Size Size
+    {
+        get => HwndInterface.GetHwndSize(Hwnd);
+        set => HwndInterface.SetHwndSize(Hwnd, value.Width, value.Height);
+    }
+
+    public string Text
+    {
+        get => HwndInterface.GetHwndText(Hwnd);
+        set => HwndInterface.SetHwndText(Hwnd, value);
+    }
+
+    public string Title
+    {
+        get => HwndInterface.GetHwndTitle(Hwnd);
+        set => HwndInterface.SetHwndTitle(Hwnd, value);
+    }
+
+    public void Click()
+    {
+        HwndInterface.ClickHwnd(Hwnd);
+    }
+
+    // <summary>
+    // Bring this window to the foreground
+    // </summary>
+    public bool Activate()
+    {
+        return HwndInterface.ActivateWindow(Hwnd);
+    }
+
+    // <summary>
+    // Minimize this window
+    // </summary>
+    public bool Minimize()
+    {
+        return HwndInterface.MinimizeWindow(Hwnd);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == typeof(HwndObject) && Equals((HwndObject)obj);
+    }
+
+    public bool Equals(HwndObject obj)
+    {
+        if (obj is null) return false;
+        return ReferenceEquals(this, obj) || obj.Hwnd.Equals(Hwnd);
+    }
+
+    public HwndObject GetChild(string cls, string title)
+    {
+        return new HwndObject(HwndInterface.GetHwndChild(Hwnd, cls, title));
+    }
+
+    public List<HwndObject> GetChildren()
+    {
+        var list = new List<HwndObject>();
+        foreach (var ptr in EnumChildren(Hwnd)) list.Add(new HwndObject(ptr));
+        return list;
+    }
+
+    public override int GetHashCode()
+    {
+        return Hwnd.GetHashCode();
+    }
+
+    public int GetMessageInt(WM msg)
+    {
+        return HwndInterface.GetMessageInt(Hwnd, msg);
+    }
+
+    public string GetMessageString(WM msg, uint param)
+    {
+        return HwndInterface.GetMessageString(Hwnd, msg, param);
+    }
+
+    public HwndObject GetParent()
+    {
+        return new HwndObject(HwndInterface.GetHwndParent(Hwnd));
+    }
+
+    public static HwndObject GetWindowByTitle(string title)
+    {
+        return new HwndObject(HwndInterface.GetHwndFromTitle(title));
+    }
+
+    public static HwndObject GetWindowByClassName(string className)
+    {
+        return new HwndObject(HwndInterface.GetHwndFromClass(className));
+    }
+
+    public static List<HwndObject> GetWindows()
+    {
+        return EnumHwnds().Select(ptr => new HwndObject(ptr)).ToList();
+    }
+
+    public static bool operator ==(HwndObject a, HwndObject b)
+    {
+        if (a is null)
+            return b is null;
+        if (b is null) return a is null;
+        return a.Hwnd == b.Hwnd;
+    }
+
+    public static bool operator !=(HwndObject a, HwndObject b)
+    {
+        return !(a == b);
+    }
+
+    public void SendMessage(WM msg, uint param1, string param2)
+    {
+        HwndInterface.SendMessage(Hwnd, msg, param1, param2);
+    }
+
+    public void SendMessage(WM msg, uint param1, uint param2)
+    {
+        HwndInterface.SendMessage(Hwnd, msg, param1, param2);
+    }
+
+    private int WindowEnum(IntPtr hWnd, int lParam)
+    {
+        var threadId = HwndInterface.GetWindowThreadProcessId(hWnd, out _);
+        if (threadId == lParam) results.Add(hWnd);
+
+        return 1;
+    }
+
+    public IntPtr[] GetWindowHandlesForThread(int threadHandle)
+    {
+        results.Clear();
+        _ = HwndInterface.EnumWindows(WindowEnum, threadHandle);
+
+        return results.ToArray();
+    }
+
+    public bool IsProcessWindowed(Process externalProcess)
+    {
+        return externalProcess.MainWindowHandle != IntPtr.Zero ||
+               (from ProcessThread threadInfo in externalProcess.Threads
+                   select GetWindowHandlesForThread(threadInfo.Id)
+                   into windows
+                   where windows != null
+                   from handle in windows
+                   select handle).Any(HwndInterface.WindowVisible);
+    }
+
+    public static bool IsNotSystemProcess(Process process)
+    {
+        try
+        {
+            _ = HwndInterface.GetSecurityInfo(process.Handle, SE_OBJECT_TYPE.SE_KERNEL_OBJECT,
+                SECURITY_INFORMATION.OWNER_SECURITY_INFORMATION, out var ownerSid, out _, out _, out _, out _);
+
+            var criticalProcess = false;
+
+            HwndInterface.IsProcessCritical(process.Handle, ref criticalProcess);
+
+            return !new SecurityIdentifier(ownerSid).IsWellKnown(WellKnownSidType.LocalSystemSid) || !criticalProcess;
+
+            // The process is running under the local system account.
+            // This is a critical process, it should be listed
+            // in the "Windows processes" section.
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private static bool EnumWindow(IntPtr handle, IntPtr pointer)
+    {
+        var gch = GCHandle.FromIntPtr(pointer);
+        if (gch.Target is not List<IntPtr> list)
+            throw new InvalidCastException("GCHandle Target could not be cast as List<IntPtr>");
+        list.Add(handle);
+        //  You can modify this to check to see if you want to cancel the operation, then return a null here
+        return true;
+    }
+
+    private IEnumerable<IntPtr> GetChildWindows(IntPtr parent)
+    {
+        var result = new List<IntPtr>();
+        var listHandle = GCHandle.Alloc(result);
+        try
+        {
+            var childProc = new HwndInterface.Win32Callback(EnumWindow);
+            HwndInterface.GetEnumChildWindows(parent, childProc, GCHandle.ToIntPtr(listHandle));
+        }
+        finally
+        {
+            if (listHandle.IsAllocated)
+                listHandle.Free();
+        }
+
+        return result;
+    }
+
+    public static List<IntPtr> EnumChildren(IntPtr hwnd)
+    {
+        var zero = IntPtr.Zero;
+        var list = new List<IntPtr>();
+        do
+        {
+            zero = HwndInterface.FindWindowEx(hwnd, zero, null, null);
+            if (zero != IntPtr.Zero) list.Add(zero);
+        } while (zero != IntPtr.Zero);
+
+        return list;
+    }
+
+    public static List<IntPtr> EnumHwnds()
+    {
+        return EnumChildren(IntPtr.Zero);
+    }
+
+    public static Size GetDisplayResolution()
+    {
+        var sf = GetWindowsScreenScalingFactor(false);
+        var screenWidth = Screen.PrimaryScreen.Bounds.Width * sf;
+        var screenHeight = Screen.PrimaryScreen.Bounds.Height * sf;
+        return new Size((int)screenWidth, (int)screenHeight);
+    }
+
+    public static double GetWindowsScreenScalingFactor(bool percentage = true)
+    {
+        //Create Graphics object from the current windows handle
+        var graphicsObject = Graphics.FromHwnd(IntPtr.Zero);
+        //Get Handle to the device context associated with this Graphics object
+        var deviceContextHandle = graphicsObject.GetHdc();
+        //Call GetDeviceCaps with the Handle to retrieve the Screen Height
+        var logicalScreenHeight = HwndInterface.GetDeviceCaps(deviceContextHandle, (int)DeviceCap.VERTRES);
+        var physicalScreenHeight = HwndInterface.GetDeviceCaps(deviceContextHandle, (int)DeviceCap.DESKTOPVERTRES);
+        //Divide the Screen Heights to get the scaling factor and round it to two decimals
+        var screenScalingFactor = Math.Round(physicalScreenHeight / (double)logicalScreenHeight, 2);
+        //If requested as percentage - convert it
+        if (percentage) screenScalingFactor *= 100.0;
+        //Release the Handle and Dispose of the GraphicsObject object
+        graphicsObject.ReleaseHdc(deviceContextHandle);
+        graphicsObject.Dispose();
+        //Return the Scaling Factor
+        return screenScalingFactor;
+    }
+
+    public void ShowAllOpenWindows()
+    {
+        var resolution = GetDisplayResolution();
+
+        var windows = Process.GetProcesses().Where(IsProcessWindowed).Where(IsNotSystemProcess).ToList();
+
+        var columnWidth = resolution.Width / windows.Count;
+        var rowHeight = resolution.Height / windows.Count;
+
+        for (var i = 0; i < windows.Count; i++)
+        {
+            decimal index = i;
+            var x = Math.Round(index / 3 * columnWidth, 0);
+            var y = Math.Round(index / 2 * rowHeight, 0);
+
+            windows[i].Refresh();
+            HwndInterface.ShowWindow(windows[i].MainWindowHandle, (int)PositioningFlags.SW_SHOWNORMAL);
+            HwndInterface.MoveWindow(windows[i].MainWindowHandle, decimal.ToInt32(x), decimal.ToInt32(y), columnWidth,
+                rowHeight,
+                true);
+        }
+    }
+
+    public static void SetResolution(int iWidth, int iHeight)
+    {
+        var dm = new DEVMODE1
+        {
+            dmDeviceName = new string(new char[32]),
+            dmFormName = new string(new char[32])
+        };
+        dm.dmSize = (short)Marshal.SizeOf(dm);
+
+        if (!EnumDisplaySettingsA(null, ENUM_CURRENT_SETTINGS, ref dm))
+            MessageBox.Show("Description: Failed To Change The Resolution.", "Information",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        dm.dmPelsWidth = iWidth;
+        dm.dmPelsHeight = iHeight;
+
+        var iRet = ChangeDisplaySettings(ref dm, CDS_TEST);
+
+        if (iRet == DISP_CHANGE_FAILED)
+        {
+            MessageBox.Show("Unable to process your request");
+            MessageBox.Show("Description: Unable To Process Your Request. Sorry For This Inconvenience.",
+                "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        else
+        {
+            iRet = ChangeDisplaySettings(ref dm, CDS_UPDATEREGISTRY);
+
+            switch (iRet)
+            {
+                case DISP_CHANGE_SUCCESSFUL:
+                {
+                    MessageBox.Show(
+                        "Description: Success.",
+                        "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+
+                    //successfull change
+                }
+                case DISP_CHANGE_RESTART:
+                {
+                    MessageBox.Show(
+                        "Description: You Need To Reboot For The Change To Happen.\n If You Feel Any Problem After Rebooting Your Machine\nThen Try To Change Resolution In Safe Mode.",
+                        "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                    //windows 9x series you have to restart
+                }
+                default:
+                {
+                    MessageBox.Show("Description: Failed To Change The Resolution.", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                    //failed to change
+                }
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        var location = Location;
+        var size = Size;
+        return $"({Hwnd}) {location.X},{location.Y}:{size.Width}x{size.Height} \"{Title}\"";
+    }
+}
